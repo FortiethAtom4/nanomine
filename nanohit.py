7c84c66a777b161892a2c3e2415a0d9f9c6c96ef
Zirconium <txbbrand@gmail.com>
2024-12-31 22:03:58 -0500

    more advanced nanohit usage

--- nanohit.py
+++ nanohit.py
@@ -1,8 +1,25 @@
-import pyautogui, time
+import pyautogui, time, argparse
 
-time.sleep(5)
-while True:
-    pyautogui.click(button="left")
-    time.sleep(1)
-    pyautogui.click(button="left")
-    time.sleep(30)
+parser = argparse.ArgumentParser("nanohit")
+parser.add_argument("-d","--delay",help="Number of seconds to delay between hits. Defaults to 30 seconds.")
+
+args = parser.parse_args()
+
+def nanohit():
+    if args.delay != None:
+        try:
+            float(args.delay)
+        except ValueError:
+            print("Error: non-numerical argument provided.")
+            return
+
+    delay: float = float(args.delay) if args.delay != None else 30
+
+    time.sleep(5)
+    while True:
+        pyautogui.click(button="left")
+        time.sleep(1)
+        pyautogui.click(button="left")
+        time.sleep(delay)
+
+nanohit()
--- nanomine.py
+++ nanomine.py
@@ -2,7 +2,7 @@ import pyautogui, time, argparse
 
 parser = argparse.ArgumentParser("nanomine")
 parser.add_argument("pickaxe",choices=["wood","stone","iron","gold","diamond","netherite"],help="Pickaxe type")
-parser.add_argument("-s","--slots",choices=[f"{i}" for i in range(1,10)],help="number of picks available")
+parser.add_argument("-s","--slots",choices=[f"{i}" for i in range(1,10)],help="Number of picks in hotbar")
 args = parser.parse_args()
 # keep track of picks
 pick_durability: int = 0
@@ -16,14 +16,14 @@ match args.pickaxe:
         mine_duration = 0.9
     case "iron":
         pick_durability = 250
-        mine_duration = 0.6
+        mine_duration = 0.7
     case "gold":
         pick_durability = 32
         mine_duration = 0.4
     case "diamond":
         pick_durability = 1561
         mine_duration = 0.5
-    case "netherite":
+    case "netherite": #please don't waste your netherite on automining cobblestone
         pick_durability = 2031
         mine_duration = 0.5
 
@@ -34,14 +34,14 @@ print("Script started, waiting 5 seconds to allow for prep...")
 time.sleep(5)
 # conditions: player looking at cobblestone in generator, pickaxe in slots 1-9 (unless otherwise specified)
 for i in range (slots):
-    print(f"Mining with slot {i}")
+    print(f"Mining with pickaxe {i + 1}...")
     for i in range(pick_durability):
         pyautogui.mouseDown(button="left")
         time.sleep(mine_duration)
         pyautogui.mouseUp()
         time.sleep(2)
     # switch slots
-    print("Pickaxe used up, switching slots...")
+    print("Pickaxe used up")
     pyautogui.scroll(-1)
 
 print("Script complete")
